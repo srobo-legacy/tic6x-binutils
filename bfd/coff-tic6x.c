3c75443d3a5399a17ebedfadf84d55deb35126e5
Jeremy Morse <jmorse+git@studentrobotics.org>
2009-11-14 17:07:47 +0000

    Placeholder and includes for bfd backend

---
+++ bfd/coff-tic6x.c
@@ -0,0 +1,10 @@
+/* As binutils decided to visit the sins of the fathers upon their descendents,
+ * insert GPL header here. Hi Rob! */
+
+#include "sysdep.h"
+#include "bfd.h"
+#include "libbfd.h"
+#include "bfdlink.h"
+#include "coff/tic6x.h"
+#include "coff/internal.h"
+#include "libcoff.h"
