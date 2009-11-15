9198e5ec92ee3598776e2e7b5a44821f87fd30f9
Jeremy Morse <jmorse+git@studentrobotics.org>
2009-11-15 00:07:07 +0000

    As instructed by coff/ti.h, redefine F_LSYMS flag for TI specific coff file headers

--- bfd/coff-tic6x.c
+++ bfd/coff-tic6x.c
@@ -8,3 +8,7 @@
 #include "coff/tic6x.h"
 #include "coff/internal.h"
 #include "libcoff.h"
+
+#undef F_LSYMS
+#define F_LSYMS		F_LSYMS_TICOFF
+/* XXX - dictated by coff/ti.h, but ti's docs say F_LSYMS remains 0x8 */
