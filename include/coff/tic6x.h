a9b81f267c93336540c7818a882339ae5a8c0452
Jeremy Morse <jmorse+git@studentrobotics.org>
2009-11-15 00:06:00 +0000

    Add some initial magic numbers and #define some options for tic6x coff format

---
+++ include/coff/tic6x.h
@@ -0,0 +1,15 @@
+/* Copyright gpl blah blah */
+
+#ifndef COFF_TIC6X_H
+#define COFF_TIC6X_H
+
+/* Some ti coff documentation in spraao8 */
+
+#define TIC6X_TARGET_ID				0x99
+#define OCTETS_PER_BYTE_POWER 			1
+#define TICOFF_TARGET_ARCH			bfd_arch_tic6x
+#define TICOFF_DEFAULT_MAGIC			TICOFF2MAGIC
+
+#include "coff/ti.h"
+
+#endif /* COFF_TIC6X_H */
