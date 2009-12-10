59181f96485f81c98dcf4cf935a1fceb16c4490c
Jeremy Morse <jmorse+git@studentrobotics.org>
2009-12-10 12:39:22 +0000

    Add UNUSED macro to satisfy -Wextras unused argument warnings

--- opcodes/tic64x-dis.c
+++ opcodes/tic64x-dis.c
@@ -5,10 +5,14 @@
 #include "opcode/tic64x.h"
 #include "coff/tic64x.h"
 
+#define UNUSED(x) ((x) = (x))
+
 int
 print_insn_tic64x(bfd_vma addr, struct disassemble_info *info)
 {
 
+	UNUSED(addr);
+	UNUSED(info);
 	fprintf(stderr, "Congratulations, you've got binutils working to a "
 			"point where stuff can actually be read in! Now you "
 			"ust need to implement the disassembly part...");
@@ -20,6 +24,10 @@ tic64x_get_insn(struct disassemble_info *info, bfd_vma addr,
 		unsigned short mdata, int *size)
 {
 
+	UNUSED(info);
+	UNUSED(addr);
+	UNUSED(mdata);
+	UNUSED(size);
 	fprintf(stderr, "Congratulations, you've got binutils working to a "
 			"point where stuff can actually be read in! Now you "
 			"ust need to implement the disassembly part...");
