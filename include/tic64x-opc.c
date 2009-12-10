cf52f72d9c65aaa9d1eb2bbe5eac98c87956e712
Jeremy Morse <jmorse+git@studentrobotics.org>
2009-12-10 12:20:12 +0000

    Add tic64x-opc.c, placeholder to hold instruction/mnemonic/whatever definitions for when someone actually gets round to digesting the reference.

---
+++ include/tic64x-opc.c
@@ -0,0 +1,8 @@
+/* Copyright blah */
+
+#include "sysdep.h"
+#include "dis-asm.h"
+#include "opcode/tic64x.h"
+
+/* Register, mnemonics, instruction table etc, all to be digested from
+ * instruction documentation. Until then, placeholder */
