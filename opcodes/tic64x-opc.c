f3204fe2785fef38022f3c474359b29b2b16d57d
Jeremy Morse <jmorse+git@studentrobotics.org>
2009-12-14 21:30:02 +0000

    Add dummy opcodes table

--- opcodes/tic64x-opc.c
+++ opcodes/tic64x-opc.c
@@ -4,5 +4,7 @@
 #include "dis-asm.h"
 #include "opcode/tic64x.h"
 
-/* Register, mnemonics, instruction table etc, all to be digested from
- * instruction documentation. Until then, placeholder */
+/* Dummy table for dummy opcodes */
+struct tic64x_op_template tic64x_opcodes[] = {
+{NULL, 		0,		0}
+};
