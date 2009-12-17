938afffea398ee6c64833c9ede1302ceed8b2531
Jeremy Morse <jmorse+git@studentrobotics.org>
2009-12-17 05:55:08 +0000

    Kill TIC64X_OP_LOAD reference, no longer existant

--- opcodes/tic64x-opc.c
+++ opcodes/tic64x-opc.c
@@ -36,8 +36,7 @@ struct tic64x_register tic64x_regs[] = {
 struct tic64x_op_template tic64x_opcodes[] = {
 {"ldndw",	 0x124,		0x17C,
 	TIC64X_OP_COND | TIC64X_OP_UNIT_D | TIC64X_OP_SIDE |
-	TIC64X_OP_UNITNO | TIC64X_OP_MEMACCESS | TIC64X_OP_LOAD |
-	TIC64X_OP_MEMSZ_DWORD,
+	TIC64X_OP_UNITNO | TIC64X_OP_MEMACCESS | TIC64X_OP_MEMSZ_DWORD,
 	{ tic64x_optxt_memaccess, tic64x_optxt_dwreg, tic64x_optxt_none },
 	{
 		{tic64x_operand_dwreg,			24,		4},
