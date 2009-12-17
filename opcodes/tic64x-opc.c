a7fd042e315c89daa325bf1de8de9a3e778c4dd6
Jeremy Morse <jmorse+git@studentrobotics.org>
2009-12-17 09:57:59 +0000

    Add int, xint, long form of "add" instruction

--- opcodes/tic64x-opc.c
+++ opcodes/tic64x-opc.c
@@ -47,6 +47,18 @@ struct tic64x_op_template tic64x_opcodes[] = {
 		{tic64x_operand_invalid,		0,		0}
 	}
 },
+{"add",		0x47C,		0xFFC,
+	TIC64X_OP_UNIT_L | TIC64X_OP_COND | TIC64X_OP_SIDE |
+	TIC64X_OP_USE_XPATH | TIC64X_OP_XPATH_SRC2,
+	{ tic64x_optxt_srcreg1, tic64x_optxt_srcreg2, tic64x_optxt_dwdst},
+	{
+		{tic64x_operand_dwdst,			23,		5},
+		{tic64x_operand_srcreg1,		13,		5},
+		{tic64x_operand_srcreg2,		18,		5},
+		{tic64x_operand_invalid,		0,		0},
+		{tic64x_operand_invalid,		0,		0}
+	}
+},
 {"b",		0x10,		0x7C,
 	TIC64X_OP_COND | TIC64X_OP_UNIT_S | TIC64X_OP_SIDE |
 	TIC64X_OP_MULTI_MNEMONIC,
