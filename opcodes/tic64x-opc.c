0c24b316b7a6cbd708a4cd01e8e42b50e4608871
Jeremy Morse <jmorse+git@studentrobotics.org>
2009-12-17 10:15:51 +0000

    Add three more add instructions. Making this ~1/5 complete! For the add instruction.

--- opcodes/tic64x-opc.c
+++ opcodes/tic64x-opc.c
@@ -59,6 +59,42 @@ struct tic64x_op_template tic64x_opcodes[] = {
 		{tic64x_operand_invalid,		0,		0}
 	}
 },
+{"add",		0x438,		0xFFC,
+	TIC64X_OP_UNIT_L | TIC64X_OP_COND | TIC64X_OP_SIDE |
+	TIC64X_OP_USE_XPATH | TIC64X_OP_XPATH_SRC1,
+	{ tic64x_optxt_srcreg1, tic64x_optxt_dwsrc, tic64x_optxt_dwdst},
+	{
+		{tic64x_operand_dwdst,			23,		5},
+		{tic64x_operand_srcreg1,		13,		5},
+		{tic64x_operand_dwsrc,			18,		5},
+		{tic64x_operand_invalid,		0,		0},
+		{tic64x_operand_invalid,		0,		0}
+	}
+},
+{"add",		0x58,		0xFFC,
+	TIC64X_OP_UNIT_L | TIC64X_OP_COND | TIC64X_OP_SIDE |
+	TIC64X_OP_USE_XPATH | TIC64X_OP_XPATH_SRC2,
+	{ tic64x_optxt_sconstant, tic64x_optxt_srcreg2, tic64x_optxt_dstreg},
+	{
+		{tic64x_operand_dstreg,			23,		5},
+		{tic64x_operand_vconstant,		13,		5},
+		{tic64x_operand_srcreg2,		18,		5},
+		{tic64x_operand_invalid,		0,		0},
+		{tic64x_operand_invalid,		0,		0}
+	}
+},
+{"add",		0x418,		0xFFC,
+	TIC64X_OP_UNIT_L | TIC64X_OP_COND | TIC64X_OP_SIDE, /* no xpath today */
+	{ tic64x_optxt_sconstant, tic64x_optxt_dwsrc, tic64x_optxt_dwdst},
+	{
+		{tic64x_operand_dwdst,			23,		5},
+		{tic64x_operand_vconstant,		13,		5},
+		{tic64x_operand_dwsrc,			18,		5},
+		{tic64x_operand_invalid,		0,		0},
+		{tic64x_operand_invalid,		0,		0}
+	}
+},
+
 {"b",		0x10,		0x7C,
 	TIC64X_OP_COND | TIC64X_OP_UNIT_S | TIC64X_OP_SIDE |
 	TIC64X_OP_MULTI_MNEMONIC,
