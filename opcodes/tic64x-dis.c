7d5c37d3951e47c6cf9eea48da282d1cf25eca8a
Jeremy Morse <jmorse+git@studentrobotics.org>
2009-12-19 02:14:17 +0000

    Implement print_insn_tic64x to read and find instruction, print "????" if not found, send to print_insn.

--- opcodes/tic64x-dis.c
+++ opcodes/tic64x-dis.c
@@ -7,14 +7,34 @@
 
 #define UNUSED(x) ((x) = (x))
 
+static void print_insn(struct tic64x_op_template *templ, uint32_t opcode);
+
 int
 print_insn_tic64x(bfd_vma addr, struct disassemble_info *info)
 {
+	struct tic64x_op_template *templ;
+	bfd_byte opbuf[4];
+	uint32_t opcode;
+	int ret;
 
-	UNUSED(addr);
-	UNUSED(info);
-	fprintf(stderr, "Congratulations, you've got binutils working to a "
-			"point where stuff can actually be read in! Now you "
-			"ust need to implement the disassembly part...");
-	exit(1);
+	ret = info->read_memory_func(addr, opbuf, 4, info);
+	if (ret) {
+		info->memory_error_func(ret, addr, info);
+		return -1;
+	}
+
+	opcode = bfd_getl32(opbuf);
+
+	for (templ = tic64x_opcodes; templ->mnemonic; templ++) {
+		if ((opcode & templ->operand_mask) == templ->opcode)
+			break;
+	}
+
+	if (templ->mnemonic == NULL) {
+		info->fprintf_func(info->stream, "????");
+		return 0;
+	}
+
+	print_insn(templ, opcode);
+	return 0;
 }
