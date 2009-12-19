52b459ff0466da32b6c1c37efe9817b1e815a448
Jeremy Morse <jmorse+git@studentrobotics.org>
2009-12-19 23:52:31 +0000

    Add operand length format macro, allows mass changing of operand whitespace settings

--- opcodes/tic64x-dis.c
+++ opcodes/tic64x-dis.c
@@ -7,6 +7,8 @@
 
 #define UNUSED(x) ((x) = (x))
 
+#define OPERAND_LENGTH_FORMAT "-10"
+
 static void print_insn(struct tic64x_op_template *templ, uint32_t opcode,
 					struct disassemble_info *info);
 
