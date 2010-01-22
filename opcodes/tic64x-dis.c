65e628b2499e77add3e1b2be4e48f3bfd6333a84
Jeremy Morse <jmorse+git@studentrobotics.org>
2010-01-22 20:28:28 +0000

    Add fudge to disassemble 15 bit constants in opcodes

--- opcodes/tic64x-dis.c
+++ opcodes/tic64x-dis.c
@@ -698,6 +698,7 @@ print_op_constant(struct tic64x_op_template *t, uint32_t opcode,
 		    t->operands[i] == tic64x_operand_const5p2 ||
 		    t->operands[i] == tic64x_operand_const21 ||
 		    t->operands[i] == tic64x_operand_const16 ||
+		    t->operands[i] == tic64x_operand_const15 ||
 		    t->operands[i] == tic64x_operand_const12 ||
 		    t->operands[i] == tic64x_operand_nops) {
 			if (type == tic64x_optxt_nops &&
