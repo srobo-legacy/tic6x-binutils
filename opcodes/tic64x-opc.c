7df662498baab4998a2c36a8ad9f90c6be9b3ac7
Jeremy Morse <jmorse+git@studentrobotics.org>
2010-01-26 10:28:17 +0000

    Add correct flags for bdec - it has a scaled pcrel offset.

--- opcodes/tic64x-opc.c
+++ opcodes/tic64x-opc.c
@@ -357,7 +357,8 @@ struct tic64x_op_template tic64x_opcodes[] = {
 },
 /* B IRP and B NRP don't really fall into what's being developed right now */
 {"bdec",	0x1020,		0x1FFC,
-	TIC64X_OP_UNIT_S,
+	TIC64X_OP_UNIT_S | TIC64X_OP_CONST_SCALE | TIC64X_OP_MEMSZ_WORD |
+	TIC64X_OP_CONST_PCREL,
 	{ tic64x_optxt_sconstant, tic64x_optxt_dstreg, tic64x_optxt_none },
 	{ tic64x_operand_const10, tic64x_operand_invalid }
 },
