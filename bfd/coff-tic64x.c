eb87193e641e4cede65c0faed753324866276f72
Jeremy Morse <jmorse+git@studentrobotics.org>
2010-03-28 20:04:48 +0100

    Add tic64x doff file format record; no code yet

--- bfd/coff-tic64x.c
+++ bfd/coff-tic64x.c
@@ -231,3 +231,54 @@ const bfd_target tic64x_coff2_vec =
 	NULL,
 	&ticoff2_swap_table
 };
+
+const bfd_target tic64x_doff_vec =
+{
+	"doff-c64x",
+	bfd_target_doff_flavour,
+	BFD_ENDIAN_LITTLE,
+	BFD_ENDIAN_LITTLE,	/* Supports big too; no time right now */
+	(HAS_RELOC | EXEC_P | HAS_LINENO | HAS_DEBUG | HAS_SYMS | HAS_LOCALS
+		| DYNAMIC | WP_TEXT),
+	(SEC_ALLOC | SEC_LOAD | SEC_RELOC | SEC_READONLY | SEC_CODE | SEC_DATA
+		| SEC_HAS_CONTENTS | SEC_DEBUGGING | SEC_STRINGS | SEC_MERGE),
+	'_',
+	'/',
+	15,
+	bfd_getl64, bfd_getl_signed_64, bfd_putl64,
+	bfd_getl32, bfd_getl_signed_32, bfd_putl32,
+	bfd_getl16, bfd_getl_signed_16, bfd_putl16,
+	bfd_getl64, bfd_getl_signed_64, bfd_putl64,
+	bfd_getl32, bfd_getl_signed_32, bfd_putl32,
+	bfd_getl16, bfd_getl_signed_16, bfd_putl16,
+	
+	{
+		_bfd_dummy_target,
+		doff_object_p,
+		bfd_generic_archive_p,
+		_bfd_dummy_target
+	},
+	{
+		bfd_false,
+		doff_mkobject,
+		_bfd_generic_mkarchive,
+		bfd_false
+	},
+	{
+		bfd_false,
+		doff_write_object_contents,
+		_bfd_write_archive_contents,
+		bfd_false
+	},
+	BFD_JUMP_TABLE_GENERIC(doff),
+	BFD_JUMP_TABLE_COPY(doff),
+	BFD_JUMP_TABLE_CORE(_bfd_nocore),
+	BFD_JUMP_TABLE_ARCHIVE(_bfd_archive_doff),
+	BFD_JUMP_TABLE_SYMBOLS(doff),
+	BFD_JUMP_TABLE_RELOCS(doff),
+	BFD_JUMP_TABLE_WRITE(tic64x_doff),
+	BFD_JUMP_TABLE_LINK(doff),
+	BFD_JUMP_TABLE_DYNAMIC(_bfd_nodynamic),
+	NULL,
+	doff_backend_data
+};
