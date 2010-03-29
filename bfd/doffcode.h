dfb48dc9a04cab7d302b752774473ea8190fffc1
Jeremy Morse <jmorse+git@studentrobotics.org>
2010-03-29 15:08:24 +0100

    Add doffcode.h: dummy implementations of all backend functions
    By dummy, I mean they print that they're not implemented and then abort. The
    idea being that I can implement these functions incrementally as objdump /
    ld / whatever run into them.

---
+++ bfd/doffcode.h
@@ -0,0 +1,553 @@
+/* Following the bfd style, implementation code goes in this header, that then
+ * gets included into implementation files with customising preprocessor foo.
+ * Not that any other arch will use this */
+
+#include "sysdep.h"
+#include "bfd.h"
+/* inclusion protection please? #include "libbfd.h" */
+#include <coff/doff.h>
+
+#define UNUSED(x) ((x) = (x))
+
+static const bfd_target *
+doff_object_p(bfd *abfd)
+{
+
+	UNUSED(abfd);
+	fprintf(stderr, "Implement doff_object_p");
+	abort();
+}
+
+static bfd_boolean
+doff_mkobject(bfd *abfd)
+{
+
+	UNUSED(abfd);
+	fprintf(stderr, "Implement doff_mkobject");
+	abort();
+}
+
+static bfd_boolean
+doff_write_object_contents(bfd *abfd)
+{
+
+	UNUSED(abfd);
+	fprintf(stderr, "Implement doff_write_contents");
+	abort();
+}
+
+static bfd_boolean
+doff_close_and_cleanup(bfd *abfd)
+{
+
+	UNUSED(abfd);
+	fprintf(stderr, "Implement doff_close_and_cleanup");
+	abort();
+}
+
+static bfd_boolean
+doff_bfd_free_cached_info(bfd *abfd)
+{
+
+	UNUSED(abfd);
+	fprintf(stderr, "Implement doff_bfd_free_cached_info");
+	abort();
+}
+
+static bfd_boolean
+doff_new_section_hook(bfd *abfd, sec_ptr section)
+{
+
+	UNUSED(abfd);
+	UNUSED(section);
+	fprintf(stderr, "Implement doff_new_section_hook");
+	abort();
+}
+
+static bfd_boolean
+doff_get_section_contents(bfd *abfd, sec_ptr section, void *data,
+				file_ptr file, bfd_size_type size)
+{
+
+	UNUSED(abfd);
+	UNUSED(section);
+	UNUSED(data);
+	UNUSED(file);
+	UNUSED(size);
+	fprintf(stderr, "Implement doff_bfd_get_section_contents");
+	abort();
+}
+
+static bfd_boolean
+doff_get_section_contents_in_window(bfd *abfd, sec_ptr section,
+			bfd_window *window, file_ptr file, bfd_size_type size)
+{
+
+	UNUSED(abfd);
+	UNUSED(section);
+	UNUSED(window);
+	UNUSED(file);
+	UNUSED(size);
+	fprintf(stderr, "Implement doff_bfd_get_section_contents_in_window");
+	abort();
+}
+
+static bfd_boolean
+doff_bfd_copy_private_bfd_data(bfd *abfd, bfd *another_bfd)
+{
+
+	UNUSED(abfd);
+	UNUSED(another_bfd);
+	fprintf(stderr, "Implement doff_bfd_copy_private_bfd_data");
+	abort();
+}
+
+static bfd_boolean
+doff_bfd_merge_private_bfd_data(bfd *abfd, bfd *another_bfd)
+{
+
+	UNUSED(abfd);
+	UNUSED(another_bfd);
+	fprintf(stderr, "Implement doff_bfd_merge_private_bfd_data");
+	abort();
+}
+
+#if 0
+BFD_JUMP_TABLE_COPY initializer declines to use this
+static bfd_boolean
+doff_bfd_init_private_section_data(bfd *abfd, sec_ptr section, bfd *another_bfd,
+			sec_ptr another_section, struct bfd_link_info *info)
+{
+
+	UNUSED(abfd);
+	UNUSED(section);
+	UNUSED(another_bfd);
+	UNUSED(another_section);
+	UNUSED(info);
+	fprintf(stderr, "Implement doff_bfd_init_section_private_data");
+	abort();
+}
+#endif
+
+static bfd_boolean
+doff_bfd_copy_private_section_data(bfd *abfd, sec_ptr section, bfd *another_bfd,
+			sec_ptr another_section)
+{
+
+	UNUSED(abfd);
+	UNUSED(section);
+	UNUSED(another_bfd);
+	UNUSED(another_section);
+	fprintf(stderr, "Implement doff_bfd_copy_private_section_data");
+	abort();
+}
+
+static bfd_boolean
+doff_bfd_copy_private_symbol_data(bfd *abfd, asymbol *symbol, bfd *another_bfd,
+			asymbol *another_symbol)
+{
+
+	UNUSED(abfd);
+	UNUSED(symbol);
+	UNUSED(another_bfd);
+	UNUSED(another_symbol);
+	fprintf(stderr, "Implement doff_bfd_copy_private_symbol_data");
+	abort();
+}
+
+static bfd_boolean
+doff_bfd_copy_private_header_data(bfd *abfd, bfd *another_bfd)
+{
+
+	UNUSED(abfd);
+	UNUSED(another_bfd);
+	fprintf(stderr, "Implement doff_bfd_copy_private_header_data");
+	abort();
+}
+
+static bfd_boolean
+doff_bfd_set_private_flags(bfd *abfd, flagword what)
+{
+
+	UNUSED(abfd);
+	UNUSED(what);
+	fprintf(stderr, "Implement doff_bfd_set_private_flags");
+	abort();
+}
+
+static bfd_boolean
+doff_bfd_print_private_bfd_data(bfd *abfd, void *what)
+{
+
+	UNUSED(abfd);
+	UNUSED(what);
+	fprintf(stderr, "Implement doff_bfd_set_private_flags");
+	abort();
+}
+
+static long
+doff_get_symtab_upper_bound(bfd *abfd)
+{
+
+	UNUSED(abfd);
+	fprintf(stderr, "Implement doff_get_symtab_upper_bound");
+	abort();
+}
+
+static long
+doff_canonicalize_symtab(bfd *abfd, struct bfd_symbol **symbol)
+{
+
+	UNUSED(abfd);
+	UNUSED(symbol);
+	fprintf(stderr, "Implement doff_canonicalize_symtab");
+	abort();
+}
+
+static struct bfd_symbol *
+doff_make_empty_symbol(bfd *abfd)
+{
+
+	UNUSED(abfd);
+	fprintf(stderr, "Implement doff_make_empty_symbol");
+	abort();
+}
+
+static void
+doff_print_symbol(bfd *abfd, void *what, struct bfd_symbol *symbol,
+			bfd_print_symbol_type type)
+{
+
+	UNUSED(abfd);
+	UNUSED(what);
+	UNUSED(symbol);
+	UNUSED(type);
+	fprintf(stderr, "Implement doff_print_symbol");
+	abort();
+}
+
+static void
+doff_get_symbol_info(bfd *abfd, struct bfd_symbol *symbol, symbol_info *info)
+{
+
+	UNUSED(abfd);
+	UNUSED(symbol);
+	UNUSED(info);
+	fprintf(stderr, "Implement doff_get_symbol_info");
+	abort();
+}
+
+static bfd_boolean
+doff_bfd_is_local_label_name(bfd *abfd, const char *name)
+{
+
+	UNUSED(abfd);
+	UNUSED(name);
+	fprintf(stderr, "Implement doff_is_local_label_name");
+	abort();
+}
+
+static bfd_boolean
+doff_bfd_is_target_special_symbol(bfd *abfd, asymbol *symbol)
+{
+
+	UNUSED(abfd);
+	UNUSED(symbol);
+	fprintf(stderr, "Implement doff_is_target_special_name");
+	abort();
+}
+
+static alent *
+doff_get_lineno(bfd *abfd, struct bfd_symbol *symbol)
+{
+
+	UNUSED(abfd);
+	UNUSED(symbol);
+	fprintf(stderr, "Implement doff_get_lineno");
+	abort();
+}
+
+static bfd_boolean
+doff_find_nearest_line(bfd *abfd, struct bfd_section *section,
+			struct bfd_symbol **symbol, bfd_vma addr,
+			const char **what, const char **what2,
+			unsigned int *what3)
+{
+
+	UNUSED(abfd);
+	UNUSED(section);
+	UNUSED(symbol);
+	UNUSED(addr);
+	UNUSED(what);
+	UNUSED(what2);
+	UNUSED(what3);
+	fprintf(stderr, "Implement doff_find_nearest_line");
+	abort();
+}
+
+static bfd_boolean
+doff_find_inliner_info(bfd *abfd, const char **what, const char **what2,
+			unsigned int *what3)
+{
+
+	UNUSED(abfd);
+	UNUSED(what);
+	UNUSED(what2);
+	UNUSED(what3);
+	fprintf(stderr, "Implement doff_find_inliner_info");
+	abort();
+}
+
+static asymbol *
+doff_bfd_make_debug_symbol(bfd *abfd, void *what, unsigned long size)
+{
+
+	UNUSED(abfd);
+	UNUSED(what);
+	UNUSED(size);
+	fprintf(stderr, "Implement doff_bfd_make_debug_symbol");
+	abort();
+}
+
+static long
+doff_read_minisymbols(bfd *abfd, bfd_boolean what, void **what2,
+			unsigned int *what3)
+{
+
+	UNUSED(abfd);
+	UNUSED(what);
+	UNUSED(what2);
+	UNUSED(what3);
+	fprintf(stderr, "Implement doff_read_minisymbols");
+	abort();
+}
+
+static asymbol *
+doff_minisymbol_to_symbol(bfd *abfd, bfd_boolean what, const void *what2,
+			asymbol *minisymbol)
+{
+
+	UNUSED(abfd);
+	UNUSED(what);
+	UNUSED(what2);
+	UNUSED(minisymbol);
+	fprintf(stderr, "Implement doff_minisymbol_to_symbol");
+	abort();
+}
+
+static long
+doff_get_reloc_upper_bound(bfd *abfd, sec_ptr section)
+{
+
+	UNUSED(abfd);
+	UNUSED(section);
+	fprintf(stderr, "Implement doff_get_reloc_upper_bound");
+	abort();
+}
+
+static long
+doff_canonicalize_reloc(bfd *abfd, sec_ptr section, arelent **relocs,
+			struct bfd_symbol **symbols)
+{
+
+	UNUSED(abfd);
+	UNUSED(section);
+	UNUSED(relocs);
+	UNUSED(symbols);
+	fprintf(stderr, "Implement doff_canonicalize_reloc");
+	abort();
+}
+
+static reloc_howto_type *
+doff_bfd_reloc_type_lookup(bfd *abfd, bfd_reloc_code_real_type type)
+{
+
+	UNUSED(abfd);
+	UNUSED(type);
+	fprintf(stderr, "Implement doff_reloc_type_lookup");
+	abort();
+}
+
+static reloc_howto_type *
+doff_bfd_reloc_name_lookup(bfd *abfd, const char *name)
+{
+
+	UNUSED(abfd);
+	UNUSED(name);
+	fprintf(stderr, "Impement doff_reloc_name_lookup");
+	abort();
+}
+
+static int
+doff_sizeof_headers(bfd *abfd, struct bfd_link_info *info)
+{
+
+	UNUSED(abfd);
+	UNUSED(info);
+	fprintf(stderr, "Implement doff_sizeof_headers");
+	abort();
+}
+
+static bfd_byte *
+doff_bfd_get_relocated_section_contents(bfd *abfd, struct bfd_link_info *info,
+			struct bfd_link_order *order, bfd_byte *what,
+			bfd_boolean what2, struct bfd_symbol **what3)
+{
+
+	UNUSED(abfd);
+	UNUSED(info);
+	UNUSED(order);
+	UNUSED(what);
+	UNUSED(what2);
+	UNUSED(what3);
+	fprintf(stderr, "Implement doff_bfd_get_relocated_section_contents");
+	abort();
+}
+
+static bfd_boolean
+doff_bfd_relax_section(bfd *abfd, struct bfd_section *section,
+			struct bfd_link_info *info, bfd_boolean *what)
+{
+
+	UNUSED(abfd);
+	UNUSED(section);
+	UNUSED(info);
+	UNUSED(what);
+	fprintf(stderr, "Implement doff_bfd_relax_section");
+	abort();
+}
+
+static struct bfd_link_hash_table *
+doff_bfd_link_hash_table_create(bfd *abfd)
+{
+
+	UNUSED(abfd);
+	fprintf(stderr, "Implement doff_bfd_link_hash_table_create");
+	abort();
+}
+
+static void
+doff_bfd_link_hash_table_free(struct bfd_link_hash_table *table)
+{
+
+	UNUSED(table);
+	fprintf(stderr, "Implement doff_bfd_link_hash_table_free");
+	abort();
+}
+
+static bfd_boolean
+doff_bfd_link_add_symbols(bfd *abfd, struct bfd_link_info *info)
+{
+
+	UNUSED(abfd);
+	UNUSED(info);
+	fprintf(stderr, "Implement doff_bfd_link_add_symbols");
+	abort();
+}
+
+static void
+doff_bfd_link_just_syms(asection *section, struct bfd_link_info *info)
+{
+
+	UNUSED(section);
+	UNUSED(info);
+	fprintf(stderr, "Implement doff_bfd_link_just_syms");
+	abort();
+}
+
+static bfd_boolean
+doff_bfd_final_link(bfd *abfd, struct bfd_link_info *info)
+{
+
+	UNUSED(abfd);
+	UNUSED(info);
+	fprintf(stderr, "Implement doff_bfd_final_link");
+	abort();
+}
+
+static bfd_boolean
+doff_bfd_link_split_section(bfd *abfd, struct bfd_section *section)
+{
+
+	UNUSED(abfd);
+	UNUSED(section);
+	fprintf(stderr, "Implement doff_bfd_link_split_section");
+	abort();
+}
+
+static bfd_boolean
+doff_bfd_gc_sections(bfd *abfd, struct bfd_link_info *info)
+{
+
+	UNUSED(abfd);
+	UNUSED(info);
+	fprintf(stderr, "Implement doff_bfd_gc_sections");
+	abort();
+}
+
+static bfd_boolean
+doff_bfd_merge_sections(bfd *abfd, struct bfd_link_info *info)
+{
+
+	UNUSED(abfd);
+	UNUSED(info);
+	fprintf(stderr, "Implement doff_bfd_merge_sections");
+	abort();
+}
+
+static bfd_boolean
+doff_bfd_is_group_section(bfd *abfd, const struct bfd_section *section)
+{
+
+	UNUSED(abfd);
+	UNUSED(section);
+	fprintf(stderr, "Implement doff_bfd_is_group_section");
+	abort();
+}
+
+static bfd_boolean
+doff_bfd_discard_group(bfd *abfd, struct bfd_section *section)
+{
+
+	UNUSED(abfd);
+	UNUSED(section);
+	fprintf(stderr, "Implement doff_bfd_discard_group");
+	abort();
+}
+
+static void
+doff_section_already_linked(bfd *abfd, struct bfd_section *section,
+			struct bfd_link_info *info)
+{
+
+	UNUSED(abfd);
+	UNUSED(section);
+	UNUSED(info);
+	fprintf(stderr, "Implement doff_section_already_linked");
+	abort();
+}
+
+static bfd_boolean
+doff_bfd_define_common_symbol(bfd *abfd, struct bfd_link_info *info,
+			struct bfd_link_hash_entry *hash)
+{
+
+	UNUSED(abfd);
+	UNUSED(info);
+	UNUSED(hash);
+	fprintf(stderr, "Implement doff_bfd_define_common_symbol");
+	abort();
+}
+static bfd_boolean
+doff_set_section_contents(bfd *abfd, sec_ptr section, const void *location,
+			file_ptr file, bfd_size_type type)
+{
+
+	UNUSED(abfd);
+	UNUSED(section);
+	UNUSED(location);
+	UNUSED(file);
+	UNUSED(type);
+	fprintf(stderr, "Implement doff_set_section_contents");
+	abort();
+}
