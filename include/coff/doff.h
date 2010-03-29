9d277b1a7f0023310fd4c43a37d404b4fc44b47d
Jeremy Morse <jmorse+git@studentrobotics.org>
2010-03-29 15:10:06 +0100

    Add empty doff.h header; structs to go here.

---
+++ include/coff/doff.h
@@ -0,0 +1,3 @@
+/* Header for doff, TIs "rework" of coff for loading code via their dspbridge
+ * driver on OMAP processors. Seeing how different this is from coff, in theory
+ * this should go in its own dir, but that'd be overkill. */
