9f49a3dc0458f3c309aa8ca483fa9b08a611050b
Jeremy Morse <jmorse+git@studentrobotics.org>
2010-03-30 02:14:24 +0100

    Add internal doff section data record

--- bfd/libdoff.h
+++ bfd/libdoff.h
@@ -3,10 +3,18 @@
 #ifndef _BFD_LIBDOFF_H_
 #define _BFD_LIBDOFF_H_
 
+struct doff_section_data {
+	int stroffset;		/* Offset into str table... of name? */
+	bfd_vma prog_addr;	/* "RUN" address */
+	bfd_vma load_addr;	/* "LOAD" address */
+	int size;		/* In bytes, or AU, or whatever */
+};
+
 struct doff_tdata {
 	char *source_filename;
 
 	int num_sections;
+	struct doff_section_data *section_data;
 
 	int num_scn_names;
 	int max_num_scn_names;
