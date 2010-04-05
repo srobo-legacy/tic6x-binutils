aaf6a47dd48cac59478018fcdcfe778b79b49d2e
Jeremy Morse <jmorse+git@studentrobotics.org>
2010-04-05 14:18:40 +0100

    Add tidoff.h: here we'll declare what we need, define tidoff_swap_table.

---
+++ bfd/tidoff.h
@@ -0,0 +1,42 @@
+#define coff_SWAP_sym_in	NULL
+#define coff_SWAP_sym_out	NULL
+#define coff_SWAP_reloc_out	NULL
+#define coff_SWAP_reloc_out	NULL
+#define coff_SWAP_filehdr_out	NULL
+#define coff_SWAP_aouthdr_out	NULL
+#define coff_SWAP_scnhdr_out	NULL
+
+#define coff_SWAP_filehdr_in	NULL
+#define coff_SWAP_aouthdr_in	NULL
+#define coff_SWAP_scnhdr_in	NULL
+#define coff_SWAP_reloc_in	NULL
+
+#define bfd_pe_print_pdata	NULL
+
+static bfd_coff_backend_data tidoff_swap_table =
+{
+	coff_SWAP_aux_in, coff_SWAP_sym_in, coff_SWAP_lineno_in,
+	codff_SWAP_aux_out, coff_SWAP_sym_out, coff_SWAP_lineno_out,
+	coff_SWAP_reloc_out, coff_SWAP_filehdr_out, coff_SWAP_aouthdr_out,
+	coff_SWAP_scnhdr_out,
+
+	FILHSZ, AOUTSZ, SCNHSZ, SYMESZ, AUXESZ, RELSZ, LINESZ, FILNMLEN,
+	/* FIXME - filename lengths? */
+
+	TRUE,	/* We always have long filenames */
+	TRUE,	/* And section names */
+	NULL,	/* set long section names? */
+	12,	/* Default alignment power */
+	FALSE,	/* Force symnames in strings */
+	0,	/* debug string prefix len. ??? */
+	coff_SWAP_filehdr_in, coff_SWAP_aouthdr_in, coff_SWAP_scnhdr_in,
+	coff_SWAP_reloc_in, doff_bad_format_hook, coff_set_arch_mach_hook,
+	coff_mkobject_hook, styp_to_sec_flags, coff_set_alignment_hook,
+	coff_slurp_symbol_table, symname_in_debug_hook,
+	coff_pointerize_aux_hook, coff_print_aux, coff_reloc16_extra_cases,
+	coff_reloc16_estimate, coff_classify_symbol,
+	coff_compute_section_file_positions, coff_start_final_link,
+	coff_relocate_section, doff_rtype_to_howto, coff_adjust_symndx,
+	coff_link_add_one_symbol, coff_link_output_has_begun,
+	coff_final_link_postscript
+};
