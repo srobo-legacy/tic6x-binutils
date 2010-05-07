aff63de6e7ca380942bdf7ba64bc90fbc825f75a
Jeremy Morse <jmorse+git@studentrobotics.org>
2010-05-07 03:03:31 +0100

    Correct section indexing: -1 is absolute, 0 undefined, others 1-based

--- bfd/doff.c
+++ bfd/doff.c
@@ -929,8 +929,14 @@ doff_write_object_contents(bfd *abfd)
 						&dsym->storage_class);
 		}
 
-		/* Section index: bfd does zero based, doff is one based */
-		H_PUT_16(abfd, sym->section->target_index + 1, &dsym->scn_num);
+		/* Section index: bfd and doff are one-based, but check for
+		 * cases where have an absolute symbol */
+		if (sym->section == bfd_abs_section_ptr)
+			H_PUT_16(abfd, -1, &dsym->scn_num);
+		else
+			H_PUT_16(abfd, sym->section->target_index,
+						&dsym->scn_num);
+
 		H_PUT_32(abfd, sym->value, &dsym->value);
 
 		/* Squirt string into string table too */
