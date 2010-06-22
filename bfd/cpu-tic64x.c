b532c9fde628ed70734985850a2363d8a53d39e0
Jeremy Morse <jmorse+git@studentrobotics.org>
2010-06-22 14:28:31 +0100

    Add copyright headers

--- bfd/cpu-tic64x.c
+++ bfd/cpu-tic64x.c
@@ -1,4 +1,20 @@
-/* blah blah copyright */
+/* Assembler for TMS320C64X class of processors
+ * Copyright 2010 Jeremy Morse <jeremy.morse@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston,
+ * MA 02110-1301, USA
+ */
 
 #include "sysdep.h"
 #include "bfd.h"
--- include/coff/doff.h
+++ include/coff/doff.h
@@ -1,3 +1,21 @@
+/* Assembler for TMS320C64X class of processors
+ * Copyright 2010 Jeremy Morse <jeremy.morse@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston,
+ * MA 02110-1301, USA
+ */
+
 /* Header for doff, TIs "rework" of coff for loading code via their dspbridge
  * driver on OMAP processors. Seeing how different this is from coff, in theory
  * this should go in its own dir, but that'd be overkill. */
